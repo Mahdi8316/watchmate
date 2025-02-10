d1551b3fb019ad2b1f3cb815719c412f3cf11a7f
Mahdi8316 <mahdi.zeinali8316@gmail.com>
2025-02-10 18:12:01 +0330

    logout

--- authentication/api/urls.py
+++ authentication/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from authentication.api.views import reg_view
+from authentication.api.views import reg_view, logout_view
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
-    path('register/', reg_view)
+    path('register/', reg_view, name='register'),
+    path('logout/', logout_view, name='logout')
 ]
--- authentication/api/views.py
+++ authentication/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from authentication.api.serializer import RegSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-
+from rest_framework import status
 from authentication import models
 
 
@@ -21,3 +21,10 @@ def reg_view(request):
             data = serializer.errors
 
         return Response(data)
+
+
+@api_view(['POST',])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
